488b0ba98f5d3be7d748b3edb32f903c920f685f
Paul Walsh <paulwalsh@Pauls-MacBook-Pro.local>
2017-08-02 17:28:47 -0400

    first

---
+++ components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './Todo';
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
+  </ul>
+);
