e16794a00b2c7b5ffab8ad907ac340767e80e9ca
Paul Walsh <paulwalsh@Pauls-MacBook-Pro.local>
2017-08-02 21:13:23 -0400

    comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
 let nextTodoId = 0;
+
+//Actions are plain JavaScript objects. Actions must have a type property that indicates the type of action being performed
+
 export const addTodo = text => {
   return {
     type: 'ADD_TODO',
@@ -6,6 +9,8 @@ export const addTodo = text => {
     text,
   };
 };
+
+//Other than type, the structure of an action object is really up to you.
 
 export const setVisibilityFilter = filter => {
   return {
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,8 +1,25 @@
+//The reducer is a pure function that takes the previous state and an action, and returns the next state.
+
+// (previousState, action) => newState
+
+/*
+
+It's called a reducer because it's the type of function you would pass to Array.prototype.reduce(reducer, ?initialValue). It's very important that the reducer stays pure. Things you should never do inside a reducer:
+
+Mutate its arguments;
+Perform side effects like API calls and routing transitions;
+Call non-pure functions, e.g. Date.now() or Math.random().
+
+*/
+
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
+      // return the state with a filter added on.
       return action.filter;
     default:
+      // For now, don't handle any actions
+      // and just return the state given to us.
       return state;
   }
 };
